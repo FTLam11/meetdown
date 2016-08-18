83aa5c5d7cd95ad191a9de981434ce91e2b8c9ab
frosted_fronks <ryzingsun11@yahoo.com>
2016-08-18 14:54:53 -0500

    Add parsing method

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,17 @@
 class UserController < ApplicationController
+  def create
+    User.parse_json(params)
+
+    @user = User.new(user_params)
+
+    if @user.save
+      respond_with(@user)
+    else
+      respond_with(@user.errors.full_messages)
+    end
+  end
+
+  def user_params
+    params.require(:details).permit(:email, :username, :password_digest, :zip_code, :age)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+
+  def self.parse_json(obj)
+    p obj
+  end
 end
