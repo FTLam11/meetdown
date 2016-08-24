fc6f20dab11a5cde2c12146913c0177ee4f85d58
Mike <Michaelyao88@gmail.com>
2016-08-24 15:47:32 -0500

    add questions and ng models

--- app/assets/javascripts/finder/finderCtrl.js
+++ app/assets/javascripts/finder/finderCtrl.js
@@ -5,6 +5,9 @@ $scope.account = account
 $scope.interests = interests.interests
 $scope.user_interests = interests.user_interests
 $scope.duplicate = false
+$scope.verbs = interests.verbs
+$scope.sentences=interests.sentences
+$scope.current_verb=$scope.verbs[0]
 
 
 $scope.pushInterest = function(){
@@ -23,5 +26,18 @@ $scope.resetDuplicate = function(){
 	$scope.duplicate=false
 }
 
+$scope.setVerb = function(verb){
+	$scope.current_verb=verb
+}
+
+$scope.nextQuestion = function(verb){
+	var yo = $scope.verbs.indexOf($scope.current_verb)
+	if (yo === $scope.verbs.length-1)
+		{yo=0}
+	else
+		{yo +=1}
+	$scope.current_verb=$scope.verbs[yo]
+
+}
 
 }]);
--- app/assets/javascripts/finder/interestFactory.js
+++ app/assets/javascripts/finder/interestFactory.js
@@ -3,7 +3,15 @@ var account = angular.module('meetdown');
 account.factory('interests', function() {
   var obj = {
   	user_interests: [],
-    interests: ["yodeling","yo-yoing","yo yo ma","yoloing"]
+    interests: ["yodeling","yo-yoing","yo yo ma","yoloing"],
+    verbs:["talk about","play","listen","watch","learn","attend","like me"],
+    sentences: {"talk about":"Things I would meet people to talk about",
+    						"play":"Things I would meet people to play together",
+    						"listen":"Things I would meet people to listen to together",
+    						"watch":"Things I would meet people to watch together",
+    						"learn":"Things I would meet people to learn together",
+    						"like me":"Things that I share with other people that I would like to meet",
+    						"attend": "Things I would meet people to attend together"}
   };
 
   obj.checkAcct = function() {
