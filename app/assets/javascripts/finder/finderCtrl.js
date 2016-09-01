bf8751832799b1807397cadfee45298df41e8773
Mike <Michaelyao88@gmail.com>
2016-09-01 16:58:33 -0500

    add userTopic angular filter

--- app/assets/javascripts/finder/finderCtrl.js
+++ app/assets/javascripts/finder/finderCtrl.js
@@ -1,10 +1,24 @@
-var main = angular.module('meetdown');
+var main = angular.module('meetdown')
+.filter('filterCategories', function () {
+    return function (topics,userInterests ) {
+      var filtered = topics;
+      for (var i = 0; i < userInterests.length; i++) {
+        var index = filtered.indexOf(userInterests[i])
+        if (index > -1) {
+          filtered.splice(index, 1);
+        }
+      }
+      return filtered;
+    }
+  }
+);
 
 main.controller('FinderCtrl', ['$scope', 'interests', 'Topics', '$location','CreateInterest', 'Auth', 'GetUserTopics', function($scope, interests, Topics, $location, CreateInterest, Auth, GetUserTopics) {
 
-$scope.user_interests = interests.user_interests
 $scope.verbs = interests.verbs
 $scope.sentences=interests.sentences
+$scope.userTopics = []
+$scope.topics = []
 
 $scope.duplicate = false
 $scope.current_verb=$scope.verbs[0]
@@ -14,20 +28,22 @@ Topics.get().$promise.then(function(data) {
 });
 
 GetUserTopics.get({user_id: angular.fromJson(window.localStorage['user'])['id']}).$promise.then(function(data) {
-  $scope.userTopics = data.user_topics
+  if (data.user_topics) {
+    $scope.userTopics = data.user_topics  
+  }
 })
 
-$scope.pushInterest = function(){
-  if ($scope.user_interests.indexOf($scope.keyword) === -1) {
-    $scope.user_interests.push($scope.keyword)
-    console.log ($scope.interests)
-    $scope.keyword = ''
-    console.log(interests.user_interests)
-  }
-  else {
-    $scope.duplicate=true
-  }
-}
+// $scope.pushInterest = function(){
+//   if ($scope.user_interests.indexOf($scope.keyword) === -1) {
+//     $scope.user_interests.push($scope.keyword)
+//     console.log ($scope.interests)
+//     $scope.keyword = ''
+//     console.log(interests.user_interests)
+//   }
+//   else {
+//     $scope.duplicate=true
+//   }
+// }
 
 $scope.resetDuplicate = function(){
   $scope.duplicate=false
@@ -46,14 +62,13 @@ $scope.nextQuestion = function(verb){
   $scope.current_verb=$scope.verbs[index]
 }
 
-$scope.showTopic = function(topicid) {
-  $location.path("/topics/"+topicid);
+$scope.showTopic = function(topic) {
+  $location.path("/topics/"+topic.id);
 }
 
-$scope.createInterest = function(topicid) {
-  console.log(window.localStorage['user'])
-  CreateInterest.save({topic_id: topicid, user_id: angular.fromJson(window.localStorage['user'])['id']}).success(function(res) {
-    $scope.topic
+$scope.createInterest = function(topic) {
+  CreateInterest.save({topic_id: topic.id, user_id: angular.fromJson(window.localStorage['user'])['id']}).$promise.then(function(res) {
+  $scope.userTopics.push(topic)
   });
   // Show confirmation of added interest AND remove interest from $scope?
 }
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,6 @@
 class Topic < ApplicationRecord
-  has_many :interests
+	has_many :interests
   has_many :users, through: :interests
   has_many :posts
+  validates_uniqueness_of :user_id, scope: :topic_id
 end
