bb22cceeb05b867b20c4acd2ef164478ad514497
frosted_fronks <ryzingsun11@yahoo.com>
2016-09-02 11:34:28 -0500

    Refactor user interests function

--- app/assets/javascripts/profile/profileCtrl.js
+++ app/assets/javascripts/profile/profileCtrl.js
@@ -1,24 +1,31 @@
 var main = angular.module('meetdown');
 
-main.controller('ProfileCtrl', ['$scope', '$stateParams', '$window', 'User', 'GetUserTopics', function($scope, $stateParams, $window, User, GetUserTopics) {
+main.controller('ProfileCtrl', ['$scope', '$stateParams', '$window', 'User', 'GetUserTopics', '$location', function($scope, $stateParams, $window, User, GetUserTopics, $location) {
 setProfile()
-// $scope.user = window.localStorage.user
-
 function setProfile(){
   if ($stateParams.id) {
     User.get({user_id: $stateParams.id}).$promise.then(function(data) {
-      $scope.user = data;
-    })
-  } else {
+      $scope.user = data.user;
+    });
+    getUserData($stateParams.id);
+  } 
+  else {
     $scope.user = angular.fromJson($window.localStorage.user);
+    getUserData(angular.fromJson(window.localStorage['user'])['id']);
   }
 }
 
-GetUserTopics.get({user_id: angular.fromJson(window.localStorage['user'])['id']}).$promise.then(function(data) {
-  if (data.user_topics) {
-    $scope.userTopics = data.user_topics;
-  } else {
-    $scope.nilTopics = ["You haven't added any interests yet."];
-  };
-});
+$scope.showTopic = function(topic) {
+  $location.path("/topics/"+topic.id);
+}
+
+function getUserData(userID) {
+  GetUserTopics.get({user_id: userID}).$promise.then(function(data) {
+    if (data.user_topics) {
+      $scope.userTopics = data.user_topics;
+    } else {
+      $scope.nilTopics = ["No interests found"];
+    };
+  });
+}
 }]);
