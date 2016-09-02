f827745754ad1e4fa262bc86831f7e6ea0981501
frosted_fronks <ryzingsun11@yahoo.com>
2016-09-02 14:49:21 -0500

    Create verb and action models

---
+++ db/migrate/20160902194905_create_actions.rb
@@ -0,0 +1,10 @@
+class CreateActions < ActiveRecord::Migration[5.0]
+  def change
+    create_table :actions do |t|
+      t.integer :verb_id
+      t.integer :topic_id
+
+      t.timestamps
+    end
+  end
+end
