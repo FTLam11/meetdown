616461ba8d26555cc9482c79c8659f4d9be436c5
Mike <Michaelyao88@gmail.com>
2016-09-06 11:16:02 -0500

    add gmap service

--- app/assets/javascripts/atlas/atlasCtrl.js
+++ app/assets/javascripts/atlas/atlasCtrl.js
@@ -4,6 +4,6 @@ atlas.controller('AtlasCtrl', ['$scope', 'uiGmapGoogleMapApi', function($scope, 
   $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
 
   uiGmapGoogleMapApi.then(function(maps) {
-
+  	
   });
 }])
