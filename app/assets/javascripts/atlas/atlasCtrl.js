f7de54b567505ea1f41fd99cf12b49a556eb745b
frosted_fronks <ryzingsun11@yahoo.com>
2016-09-09 18:10:21 -0500

    Map colors to zip codes

--- app/assets/javascripts/atlas/atlasCtrl.js
+++ app/assets/javascripts/atlas/atlasCtrl.js
@@ -80,4 +80,80 @@ atlas.controller('AtlasCtrl', ['$scope', 'uiGmapGoogleMapApi', 'interests','Topi
           }
         }
       }
+
+  function computePercentiles(zipObj) {
+    var sum = 0;
+    var percentile = {};
+
+    for (var key in zipObj) {
+      sum += zipObj[key];
+    };
+    
+    for (var key in zipObj) {
+      if (zipObj.hasOwnProperty(key)) {
+        percentile[key]["percentile"] = zipObj[key] / sum
+      }
+    };
+
+    return percentile;
+  }
+
+  function PercentileToColor(percentileArr) {
+
+  }
+
+  function test(zipArr) {
+    zipKeys = Object.keys(zipArr);
+    var arr = [];
+
+    colors = [
+      "#49006A",
+      "#7A0177",
+      "#AE017E",
+      "#DD3497",
+      "#F768A1",
+      "#FA9FB5",
+      "#FCC5C0",
+      "#FDE0DD",
+      "#FFF7F3"
+    ];
+
+    chunk = Math.floor(zipArr / 9);
+    zipsThatGetLastColor = zipArr % 9;
+    currentColor = 0;
+
+    for (var i = 0; i < colors.length - 1; i++) {
+      arr.push(colorMe(zipArr[j], color[currentColor]));
+
+      for (var j = 0; j < zipArr)
+    }
+
+
+
+
+    for (var j = 0; j < chunk; j++) {
+      arr.push(colorMe(zipArr[j], color[currentColor]));
+    };
+
+    currentColor += 1;
+
+    for (var k = chunk + 1; k < zipsThatGetLastColor; k += chunk) {
+      arr.push(colorMe(zipArr[k], color[currentColor]));
+      arr.push(colorMe(zipArr[k + 1], color[currentColor]));
+      currentColor += 1;
+    };
+
+    for (var l = zipsThatGetLastColor; l < zipArr.length; l++) {
+      arr.push(colorMe(zipArr[l], color[-1]));
+    };
+  };
+
+  function colorMe(zipcode, color) {
+    {
+      where: 'Zipcode = '+ zipcode,
+      polygonOptions: {
+        fillColor: color;
+      }
+    }
+  }
 }])
