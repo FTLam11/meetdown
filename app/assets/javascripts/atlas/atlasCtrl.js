b18a39f58398c85809e6c60dd5fa1a574aa0104b
frosted_fronks <ryzingsun11@yahoo.com>
2016-09-09 19:05:24 -0500

    Debug mapping of zip codes

--- app/assets/javascripts/atlas/atlasCtrl.js
+++ app/assets/javascripts/atlas/atlasCtrl.js
@@ -24,9 +24,7 @@ atlas.controller('AtlasCtrl', ['$scope', 'uiGmapGoogleMapApi', 'interests','Topi
 
       zipString = zipString.slice(0,-1) + ")"
       setLayer(zipString)
-
-      
-    }
+    } 
   }
 
   GetUserTopics.get({user_id: angular.fromJson(window.localStorage['user'])['id']}).$promise.then(function(data) {
@@ -50,61 +48,41 @@ atlas.controller('AtlasCtrl', ['$scope', 'uiGmapGoogleMapApi', 'interests','Topi
 
   function setLayer(zipString) {
     $scope.map.fusionlayer = {
-          options: {
-            heatmap: {
-              enabled: false
-            },
-            query: {
-              select: "geometry",
-              from: "1n9XBy8dml7ZGNt65-m8XBYnvXIPaImQnDDlMKum6",
-              where: "Zipcode IN " + zipString
-            },
-            options: {
-              styleId: 2,
-              templateId: 3
-            },
-            // styles: [{
-            //   where: 'Zipcode != '+'60089',
-            //   polygonOptions: {}
-            // }, {
-            //   where: 'Zipcode = '+'60089',
-            //   polygonOptions: {
-            //     fillColor: '#0000FF'
-            //   }
-            // }, {
-            //   where: 'population > 5',
-            //   polygonOptions: {
-            //     fillOpacity: 1.0
-            //   }
-            // }]
-          }
-        }
+      options: {
+        heatmap: {
+          enabled: false
+        },
+        query: {
+          select: "geometry",
+          from: "1n9XBy8dml7ZGNt65-m8XBYnvXIPaImQnDDlMKum6",
+          where: "Zipcode IN " + zipString
+        },
+        options: {
+          styleId: 2,
+          templateId: 3
+        },
+        // styles: [{
+        //   where: 'Zipcode != '+'60089',
+        //   polygonOptions: {}
+        // }, {
+        //   where: 'Zipcode = '+'60089',
+        //   polygonOptions: {
+        //     fillColor: '#0000FF'
+        //   }
+        // }, {
+        //   where: 'population > 5',
+        //   polygonOptions: {
+        //     fillOpacity: 1.0
+        //   }
+        // }]
       }
-
-  function computePercentiles(zipObj) {
-    var sum = 0;
-    var percentile = {};
-
-    for (var key in zipObj) {
-      sum += zipObj[key];
-    };
-    
-    for (var key in zipObj) {
-      if (zipObj.hasOwnProperty(key)) {
-        percentile[key]["percentile"] = zipObj[key] / sum
-      }
-    };
-
-    return percentile;
+    }
+    console.log($scope.map.fusionlayer);
   }
 
-  function PercentileToColor(percentileArr) {
-
-  }
-
-  function test(zipArr) {
+  function setZipColorQuery(zipArr) {
     zipKeys = Object.keys(zipArr);
-    var arr = [];
+    var queryArr = [];
 
     colors = [
       "#49006A",
@@ -118,42 +96,31 @@ atlas.controller('AtlasCtrl', ['$scope', 'uiGmapGoogleMapApi', 'interests','Topi
       "#FFF7F3"
     ];
 
-    chunk = Math.floor(zipArr / 9);
-    zipsThatGetLastColor = zipArr % 9;
+    chunk = Math.floor(zipKeys.length / 9);
+    zipsThatGetLastColor = zipKeys.length % 9;
     currentColor = 0;
 
-    for (var i = 0; i < colors.length - 1; i++) {
-      arr.push(colorMe(zipArr[j], color[currentColor]));
-
-      for (var j = 0; j < zipArr)
-    }
-
-
-
-
-    for (var j = 0; j < chunk; j++) {
-      arr.push(colorMe(zipArr[j], color[currentColor]));
-    };
-
-    currentColor += 1;
-
-    for (var k = chunk + 1; k < zipsThatGetLastColor; k += chunk) {
-      arr.push(colorMe(zipArr[k], color[currentColor]));
-      arr.push(colorMe(zipArr[k + 1], color[currentColor]));
+    for (var k = 0; k < zipKeys.length - zipsThatGetLastColor; k += chunk) {
+      queryArr.push(colorMe(zipKeys[k], color[currentColor]));
+      queryArr.push(colorMe(zipKeys[k + 1], color[currentColor]));
       currentColor += 1;
     };
 
-    for (var l = zipsThatGetLastColor; l < zipArr.length; l++) {
-      arr.push(colorMe(zipArr[l], color[-1]));
+    for (var l = zipsThatGetLastColor; l < zipKeys.length; l++) {
+      queryArr.push(colorMe(zipKeys[l], color[-1]));
     };
+
+    $scope.map.fusionlayer.options["styles"] = queryArr;
   };
 
   function colorMe(zipcode, color) {
-    {
+    obj = {
       where: 'Zipcode = '+ zipcode,
       polygonOptions: {
-        fillColor: color;
+        fillColor: color
       }
-    }
+    };
+
+    return obj;
   }
 }])
