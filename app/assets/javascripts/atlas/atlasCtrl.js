be4cba96b3bfaa984f7f4754e6eae65f74980c54
frosted_fronks <ryzingsun11@yahoo.com>
2016-09-11 11:43:07 -0500

    Extract .map to service

--- app/assets/javascripts/atlas/atlasCtrl.js
+++ app/assets/javascripts/atlas/atlasCtrl.js
@@ -1,68 +1,11 @@
 var atlas = angular.module('meetdown')
 
-atlas.controller('AtlasCtrl', ['$scope', 'uiGmapGoogleMapApi', 'interests', 'Topics', 'GetUserTopics', 'ZipCount', function($scope, uiGmapGoogleMapApi, interests, Topics, GetUserTopics, ZipCount) {
+atlas.controller('AtlasCtrl', ['$scope', 'uiGmapGoogleMapApi', 'interests', 'Topics', 'GetUserTopics', 'ZipCount', 'StyleMap', function($scope, uiGmapGoogleMapApi, interests, Topics, GetUserTopics, ZipCount, StyleMap) {
     $scope.topics = [];
     $scope.userTopics = [];
     $scope.queryTopic = "";
     $scope.showFusionLayer = true;
-    $scope.map = {
-        center: { latitude: 42, longitude: -88 },
-        options: { minZoom: 3, maxZoom: 13 },
-        zoom: 9,
-        styles:  [{
-          "featureType": "administrative",
-          "elementType": "labels.text.fill",
-          "stylers": [{
-              "color": "#444444"
-          }]
-        }, {
-          "featureType": "landscape",
-          "elementType": "all",
-          "stylers": [{
-              "color": "#f2f2f2"
-          }]
-        }, {
-          "featureType": "poi",
-          "elementType": "all",
-          "stylers": [{
-              "visibility": "off"
-          }]
-        }, {
-          "featureType": "road",
-          "elementType": "all",
-          "stylers": [{
-              "saturation": -100
-          }, {
-              "lightness": 45
-          }]
-        }, {
-          "featureType": "road.highway",
-          "elementType": "all",
-          "stylers": [{
-              "visibility": "simplified"
-          }]
-        }, {
-          "featureType": "road.arterial",
-          "elementType": "labels.icon",
-          "stylers": [{
-              "visibility": "off"
-          }]
-        }, {
-          "featureType": "transit",
-          "elementType": "all",
-          "stylers": [{
-              "visibility": "off"
-          }]
-        }, {
-          "featureType": "water",
-          "elementType": "all",
-          "stylers": [{
-              "color": "#89d1d3"
-          }, {
-              "visibility": "on"
-          }]
-      }]
-    }
+    $scope.map = StyleMap;
     $scope.map.fusionlayer = {};
 
     $scope.setQueryTopic = function(topic) {
--- app/assets/javascripts/services.js
+++ app/assets/javascripts/services.js
@@ -45,6 +45,67 @@ angular.module('meetdown')
   return $resource("/users/:id", {id: "@id"}, {update: {method: 'PUT'}})
 })
 
+.service('StyleMap', function() {
+  return {
+        center: { latitude: 42, longitude: -88 },
+        options: { minZoom: 3, maxZoom: 13 },
+        zoom: 9,
+        styles:  [{
+          "featureType": "administrative",
+          "elementType": "labels.text.fill",
+          "stylers": [{
+              "color": "#444444"
+          }]
+        }, {
+          "featureType": "landscape",
+          "elementType": "all",
+          "stylers": [{
+              "color": "#f2f2f2"
+          }]
+        }, {
+          "featureType": "poi",
+          "elementType": "all",
+          "stylers": [{
+              "visibility": "off"
+          }]
+        }, {
+          "featureType": "road",
+          "elementType": "all",
+          "stylers": [{
+              "saturation": -100
+          }, {
+              "lightness": 45
+          }]
+        }, {
+          "featureType": "road.highway",
+          "elementType": "all",
+          "stylers": [{
+              "visibility": "simplified"
+          }]
+        }, {
+          "featureType": "road.arterial",
+          "elementType": "labels.icon",
+          "stylers": [{
+              "visibility": "off"
+          }]
+        }, {
+          "featureType": "transit",
+          "elementType": "all",
+          "stylers": [{
+              "visibility": "off"
+          }]
+        }, {
+          "featureType": "water",
+          "elementType": "all",
+          "stylers": [{
+              "color": "#89d1d3"
+          }, {
+              "visibility": "on"
+          }]
+      }]
+    };
+})
+
 .service('Test', function($http){
   return {
    test: function(data) {
