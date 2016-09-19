bfc48400587156c1b548f4591bba82bd501e3894
frosted_fronks <ryzingsun11@yahoo.com>
2016-09-19 16:15:03 -0500

    Remove migration and add email to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160919203317) do
+ActiveRecord::Schema.define(version: 20160914033848) do
 
   create_table "attendings", force: :cascade do |t|
     t.integer  "user_id"
@@ -65,40 +65,19 @@ ActiveRecord::Schema.define(version: 20160919203317) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
-    t.string   "password"
+    t.string   "email"
+    t.string   "password_digest"
     t.integer  "zip_code"
     t.integer  "age"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "fb_id",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.integer  "failed_attempts",        default: 0,  null: false
-    t.string   "unlock_token"
-    t.datetime "locked_at"
     t.string   "phone_number"
     t.string   "google_id"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email"
-    t.index ["fb_id"], name: "index_users_on_fb_id"
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
   create_table "zipcodes", force: :cascade do |t|
     t.string   "zipcode"
+    t.string   "center"
     t.string   "geojson"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
