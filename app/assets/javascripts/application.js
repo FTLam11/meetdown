8112424ef2092c4ab3f6b07298e65518422a6399
frosted_fronks <ryzingsun11@yahoo.com>
2016-09-27 07:25:05 -0500

    Remove EZFB dependencies

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,7 +2,6 @@
 //= require angular
 //= require angular-rails-templates
 //= require angular-ui-router
-//= require angular-easyfb
 //= require angular-highlightjs
 //= require angular-resource
 //= require lodash/lodash
