bf428beb1fb6278dcdaefe4d75e8288be5a6a087
frosted_fronks <ryzingsun11@yahoo.com>
2016-10-05 14:51:30 -0500

    Add method to determine neighboring zip codes

--- app/models/zipcode.rb
+++ app/models/zipcode.rb
@@ -1,5 +1,8 @@
 class Zipcode < ApplicationRecord
-	has_many :neighbors
-	has_many :proximities, through: :neighbors, source: :zipcode
+  has_many :neighbors
+  has_many :proximities, through: :neighbors, source: :zipcode
 
+  def neighbors
+    Neighbor.where(["zipcode_1 = ? or zipcode_2 = ?", self.zipcode, self.zipcode]).map {|zipcode_pair| zipcode_pair = [zipcode_pair.zipcode_1, zipcode_pair.zipcode_2]}.flatten.select {|zipcode| zipcode != self.zipcode}
+  end
 end
