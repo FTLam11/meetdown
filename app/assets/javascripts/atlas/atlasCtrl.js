fd30a3b42d8b69a93f0d128e6655e6bc285bbddf
frosted_fronks <ryzingsun11@yahoo.com>
2016-10-12 16:29:48 -0500

    Extract heatmap function to AtlasFactory

--- app/assets/javascripts/atlas/atlasCtrl.js
+++ app/assets/javascripts/atlas/atlasCtrl.js
@@ -1,6 +1,6 @@
 var atlas = angular.module('meetdown')
 
-atlas.controller('AtlasCtrl', ['$scope', 'uiGmapGoogleMapApi', 'Topics', 'GetUserTopics', 'ZipCount', 'StyleMap', 'GetZipTopics', 'Topic', '$auth', function($scope, uiGmapGoogleMapApi, Topics, GetUserTopics, ZipCount, StyleMap, GetZipTopics, Topic, $auth) {
+atlas.controller('AtlasCtrl', ['$scope', 'uiGmapGoogleMapApi', 'Topics', 'GetUserTopics', 'ZipCount', 'StyleMap', 'GetZipTopics', 'Topic', '$auth', 'AtlasFactory', function($scope, uiGmapGoogleMapApi, Topics, GetUserTopics, ZipCount, StyleMap, GetZipTopics, Topic, $auth, AtlasFactory) {
   $scope.topics = [];
   $scope.showFusionLayer = true;
   $scope.map = StyleMap;
@@ -9,7 +9,7 @@ atlas.controller('AtlasCtrl', ['$scope', 'uiGmapGoogleMapApi', 'Topics', 'GetUse
   $scope.currentZip = "60654";
 
   if ($auth.getPayload() != undefined && $auth.getPayload().zip_code != undefined) {
-    $scope.currentZip = $auth.getPayload()['zip_code'];
+    $scope.currentZip = $auth.getPayload().zip_code;
   };
 
   $scope.setCurrentZip = function(zipcode) {
@@ -25,7 +25,7 @@ atlas.controller('AtlasCtrl', ['$scope', 'uiGmapGoogleMapApi', 'Topics', 'GetUse
 
   $scope.d3color = function() {
     return function(d, i) {
-      return COLORS[i];
+      return AtlasFactory.COLORS[i];
     };
   };
 
@@ -43,88 +43,11 @@ atlas.controller('AtlasCtrl', ['$scope', 'uiGmapGoogleMapApi', 'Topics', 'GetUse
 
   $scope.setQueryTopic = function(topic) {
     ZipCount.get({ id: topic.id }).$promise.then(function(data) {
-      plotHeatmap(data);
+      AtlasFactory.plotHeatmap(data);
     });
   };
-
-  function plotHeatmap(data) {
-    if (Object.keys(data.zip_codes).length > 0) {
-      var zipString = "(";
-      for (var key in data.zip_codes) {
-        zipString += key + ",";
-      }
-
-      zipString = zipString.slice(0, -1) + ")";
-      setLayer(zipString);
-      setZipColorQuery(data.zip_codes);
-      $scope.showFusionLayer = true;
-    } else {
-      $scope.showFusionLayer = false;
-    }
-  }; //service?
 
   Topics.get().$promise.then(function(data) {
     $scope.topics = data.topics;
   });
-
-  function setLayer(zipString) {
-    $scope.map.fusionlayer = {
-      options: {
-        heatmap: {
-          enabled: false
-        },
-        query: {
-          select: "geometry",
-          from: "1n9XBy8dml7ZGNt65-m8XBYnvXIPaImQnDDlMKum6",
-          where: "Zipcode IN " + zipString
-        }
-      }
-    };
-  }; //service?
-
-  const COLORS = [
-    "#49006A",
-    "#7A0177",
-    "#AE017E",
-    "#DD3497",
-    "#F768A1",
-    "#FA9FB5",
-    "#FCC5C0",
-    "#FDE0DD",
-    "#FFF7F3"
-  ]; //factory
-
-  function setZipColorQuery(zipObj) {
-    var zipKeys = Object.keys(zipObj);
-    var queryArr = [];
-    var chunk = Math.floor(zipKeys.length / 8);
-    var zipsThatGetLastColor = zipKeys.length % 8;
-    var currentColor = 0;
-    var arrNum = 0;
-
-    if (chunk > 0) {
-      for (var colorNum = 0; colorNum < 7; colorNum++) {
-        for (var chunkNum = 0; chunkNum < chunk; chunkNum++) {
-          queryArr.push(colorMe(zipKeys[arrNum], COLORS[colorNum]));
-          arrNum++;
-        };
-      };
-    }
-
-    for (; arrNum < zipKeys.length; arrNum++) {
-      queryArr.push(colorMe(zipKeys[arrNum], COLORS[COLORS.length - 1]));
-    };
-
-    $scope.map.fusionlayer.options["styles"] = queryArr;
-  }; //service?
-
-  function colorMe(zipcode, color) {
-    var obj = {
-      where: 'Zipcode = ' + zipcode,
-      polygonOptions: {
-        fillColor: color
-      }
-    };
-    return obj;
-  }; //factory
 }])
--- app/assets/javascripts/atlas/atlasFactory.js
+++ app/assets/javascripts/atlas/atlasFactory.js
@@ -1,5 +1,84 @@
 var atlas = angular.module('meetdown');
 
-atlas.factory('atlasFactory', function() {
+atlas.factory('AtlasFactory', function() {
+  var obj = {};
   
-})
+  obj.COLORS = [
+    "#49006A",
+    "#7A0177",
+    "#AE017E",
+    "#DD3497",
+    "#F768A1",
+    "#FA9FB5",
+    "#FCC5C0",
+    "#FDE0DD",
+    "#FFF7F3"
+  ];
+
+  obj.plotHeatmap = function(data) {
+    if (Object.keys(data.zip_codes).length > 0) {
+      var zipString = "(";
+      for (var key in data.zip_codes) {
+        zipString += key + ",";
+      }
+
+      zipString = zipString.slice(0, -1) + ")";
+      setLayer(zipString);
+      setZipColorQuery(data.zip_codes);
+      $scope.showFusionLayer = true;
+    } else {
+      $scope.showFusionLayer = false;
+    }
+  };
+
+  obj.setLayer = function(zipString) {
+    $scope.map.fusionlayer = {
+      options: {
+        heatmap: {
+          enabled: false
+        },
+        query: {
+          select: "geometry",
+          from: "1n9XBy8dml7ZGNt65-m8XBYnvXIPaImQnDDlMKum6",
+          where: "Zipcode IN " + zipString
+        }
+      }
+    };
+  };
+
+  obj.setZipColorQuery = function(zipObj) {
+    var zipKeys = Object.keys(zipObj);
+    var queryArr = [];
+    var chunk = Math.floor(zipKeys.length / 8);
+    var zipsThatGetLastColor = zipKeys.length % 8;
+    var currentColor = 0;
+    var arrNum = 0;
+
+    if (chunk > 0) {
+      for (var colorNum = 0; colorNum < 7; colorNum++) {
+        for (var chunkNum = 0; chunkNum < chunk; chunkNum++) {
+          queryArr.push(colorMe(zipKeys[arrNum], COLORS[colorNum]));
+          arrNum++;
+        };
+      };
+    }
+
+    for (; arrNum < zipKeys.length; arrNum++) {
+      queryArr.push(colorMe(zipKeys[arrNum], COLORS[COLORS.length - 1]));
+    };
+
+    $scope.map.fusionlayer.options["styles"] = queryArr;
+  };
+
+  obj.colorMe = function(zipcode, color) {
+      var obj = {
+        where: 'Zipcode = ' + zipcode,
+        polygonOptions: {
+          fillColor: color
+        }
+      };
+      return obj;
+  };
+
+  return obj;
+});
