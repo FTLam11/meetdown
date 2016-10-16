40bef834f475b738affb282f0910f0137436673f
frosted_fronks <ryzingsun11@yahoo.com>
2016-10-16 12:24:22 -0500

    Remove old topics from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,3 @@
-# # Topic.create(name: "Pokemon", verbs:"1100110")
-# # Topic.create(name: "Magic: The Gathering", verbs:"1000110")
-# # Topic.create(name: "Dota 2", verbs:"1101000")
-# # Topic.create(name: "Dominion Online", verbs:"0100110")
-# # Topic.create(name: "Chicago Cubs", verbs:"0000110")
-# # Topic.create(name: "Jasmine Tea", verbs:"1110000")
-# # Topic.create(name: "Eevee", verbs:"0000111")
-# # Topic.create(name: "BBQ Bacon Cheeseburger", verbs:"1010110")
-# # Topic.create(name: "Grapefruit", verbs:"0100110")
-# # Topic.create(name: "Sky Diving", verbs:"1111111")
-
-# # User.create(email: "",fb_id: "2")
-
 # # require 'rubygems'
 # # require 'json'
 
