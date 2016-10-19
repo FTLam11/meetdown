6d6770bf653414cb6783ab84fbd6c29e43ef4fa3
frosted_fronks <ryzingsun11@yahoo.com>
2016-10-19 14:47:29 -0500

    Try alternate koala methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
     @oauth = Koala::Facebook::OAuth.new(Rails.application.secrets.fb_client_id, Rails.application.secrets.fb_secret_key, params[:redirectUri])
     # oauthtoken = @oauth.get_access_token(params[:code])
     @oauth.url_for_oauth_code
-    @oauth.get_access_token(code)
+    oauthtoken = @oauth.get_access_token(code)
     @graph = Koala::Facebook::API.new(oauthtoken)
     profile = @graph.get_object("me")
     payload = User.find_or_create_by(fb_id: profile["id"]).as_json
