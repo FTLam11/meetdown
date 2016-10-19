2845e187180bee9742d6f9dfcce5c7f7cb332bf9
frosted_fronks <ryzingsun11@yahoo.com>
2016-10-19 17:29:33 -0500

    Remove profile pic from survey

--- app/assets/javascripts/survey/surveyCtrl.js
+++ app/assets/javascripts/survey/surveyCtrl.js
@@ -1,46 +1,11 @@
 var survey = angular.module('meetdown');
 
-survey.controller('surveyCtrl', ['$scope', 'SubmitSurvey', '$state', '$location','$window', '$auth', 'RequestSignature', 'UploadToS3', 'DataURItoBlob', 'Authenticate','$uibModalInstance', function($scope, SubmitSurvey, $state, $location, $window, $auth, RequestSignature, UploadToS3, DataURItoBlob, Authenticate, $uibModalInstance) {
-
-$scope.interests = []
+survey.controller('surveyCtrl', ['$scope', 'SubmitSurvey', '$auth', '$uibModalInstance', function($scope, SubmitSurvey, $auth, $uibModalInstance) {
 Authenticate();
 $scope.submitInfo = function() {
-    SubmitSurvey.update({age: $scope.age, zip_code: $scope.zipcode, id: $auth.getPayload().id, token: $auth.getToken()}).$promise.then(function(response){
+    SubmitSurvey.update({zip_code: $scope.zipcode, id: $auth.getPayload().id, token: $auth.getToken()}).$promise.then(function(response){
       $auth.setToken(response.token);
-      $uibModalInstance.close()
+      $uibModalInstance.close();
     })
-};
-
-$scope.showProfile = function(profileID) {
-  $location.path("/profile/" + profileID)
-};
-
-$scope.profilePic = "";
-$scope.croppedProfilePic = "";
-
-var fileManager = function(event) {
-  var file = event.currentTarget.files[0];
-  var reader = new FileReader();
-  reader.onload = function(event) {
-    $scope.$apply(function($scope) {
-      $scope.profilePic = event.target.result;
-    });
-  };
-  reader.readAsDataURL(file);
-};
-
-angular.element(document.querySelector('#fileInput')).on('change', fileManager);
-
-$scope.updateProfilePic = function() {
-  RequestSignature.save({token: $auth.getToken(), key: $scope.file.name, user: $auth.getPayload()}).$promise.then(function(response) {
-    UploadToS3.upload({key: response.key, AWSAccessKeyId: response.AWSAccessKeyId, acl: "public-read", policy: response.policy, signature: response.signature, 'Content-Type': 'image/jpeg', file: $scope.croppedProfilePicBlob}).$promise.then(function(s3Response) {
-      SubmitSurvey.update({id: $auth.getPayload().id, picture: "https://s3.amazonaws.com/media.meetdown.info/" + s3Response.key, token: $auth.getToken()}).$promise.then(function(response){
-        $auth.setToken(response.token);
-        $scope.showProfile($auth.getPayload()['id']);
-      })
-    });
-  });
-
-$scope.croppedProfilePicBlob = DataURItoBlob($scope.croppedProfilePic);
 };
 }]);
