ef0e92b3f7d9035b3bb628547f7c12bba8b30d40
frosted_fronks <ryzingsun11@yahoo.com>
2016-10-26 11:17:59 -0500

    Style zipcode modal and add cancel function in surveyCtrl

--- app/assets/javascripts/survey/surveyCtrl.js
+++ app/assets/javascripts/survey/surveyCtrl.js
@@ -2,10 +2,15 @@ var survey = angular.module('meetdown');
 
 survey.controller('surveyCtrl', ['$scope', 'SubmitSurvey', '$auth', '$uibModalInstance', 'Authenticate', function($scope, SubmitSurvey, $auth, $uibModalInstance, Authenticate) {
 Authenticate();
+
 $scope.submitInfo = function() {
     SubmitSurvey.update({zip_code: $scope.zipcode, id: $auth.getPayload().id, token: $auth.getToken()}).$promise.then(function(response){
       $auth.setToken(response.token);
       $uibModalInstance.close();
     })
 };
+
+$scope.cancel = function() {
+  $uibModalInstance.close();
+}
 }]);
