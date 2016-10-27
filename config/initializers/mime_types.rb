a70337a3714f00ab2e24d4c34770753897d3b8cf
frosted_fronks <ryzingsun11@yahoo.com>
2016-10-27 17:09:05 -0500

    Register mime types for font awesome

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,3 +2,7 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
+Mime::Type.register "application/font-woff", :woff
+Mime::Type.register "application/font-woff2", :woff2
+Mime::Type.register "application/octet-stream", :ttf
+
